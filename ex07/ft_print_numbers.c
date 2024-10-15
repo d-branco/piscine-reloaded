e73a5fffcca06f246a667bab1e406b32a3b54074
Diniz <abessa-m@c1r2s7.42porto.com>
2024-10-15 16:38:51 +0100

    ex07

--- ex07/ft_print_numbers.c
+++ ex07/ft_print_numbers.c
@@ -6,14 +6,28 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 16:32:42 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/15 16:35:24 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/15 16:38:37 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 void	ft_print_numbers(void);
 
+/*
+int	main(void)
+{
+	ft_print_numbers();
+}
+*/
+
 void	ft_print_numbers(void)
 {
-	
+	char	digit;
+
+	digit = '0';
+	while (digit <= '9')
+	{
+		ft_putchar(digit);
+		digit++;
+	}
 }
