4dc2e18376f73794d6077dfc87c248d9dae4e4ea
Diniz <185072671+d-branco@users.noreply.github.com>
2024-10-17 15:44:09 +0100

    printing arrays of ints

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 13:35:49 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/17 14:35:53 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/17 15:42:23 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,32 @@ int	*ft_range(int min, int max);
 //////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 
-/*print_array_of_ints(int *vector)
-{
-	
-}*/
+void	ad_print_array(int size, int *vector);
 
 int	main(void)
 {
 	int	*ptr;
+	int test[5] = {10, 20, 30, 40, 50};
+
+	ad_print_array(5, test);
 
 	ptr = ft_range(1, 0);
-	//print_array_of_ints(ft_range(0, 0));
+	ad_print_array(0, ptr);
+}
+
+void	ad_print_array(int size, int *vector)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		printf("%i", vector[i]);
+		i++;
+		if (i < size)
+			printf(", ");
+	}
+	printf("\n");
 }
 //////////////////////////////////////////////////////////////////////////////
 
