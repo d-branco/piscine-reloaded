42306e48723cc845da2b272dc9d24d9dd95cf168
Diniz <abessa-m@c1r2s7.42porto.com>
2024-10-17 13:15:52 +0100

    ex19 norminette

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,36 +6,59 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:40:42 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/17 10:17:32 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:15:27 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_putchar(char c);
 void	print_argv(int argc, char **argv);
+int		ft_strcmp(char *s1, char *s2);
 
 int	main(int argc, char **argv)
 {
 	int		i;
-	char	*strptr[argc - 1];
+	char	*temp;
 
 	i = 1;
-	while (i < argc)
+	while (i < (argc - 1))
 	{
-		strptr[i - 1] = argv[i];
+		if (ft_strcmp(argv[i + 1], argv[i]) < 0)
+		{
+			temp = argv[i + 1];
+			argv[i + 1] = argv[i];
+			argv[i] = temp;
+			i -= 2;
+		}
 		i++;
 	}
-
 	print_argv(argc, argv);
 }
 
 void	print_argv(int argc, char **argv)
 {
 	int	i;
-	
-	i = 0;
-	while (i < argc - 1)
+	int	j;
+
+	i = 1;
+	while (i < argc)
 	{
-		ft_put_str(strptr[i]);
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
 		ft_putchar('\n');
 		i++;
 	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+		i++;
+	return (s1[i] - s2[i]);
 }
