6cc0caf88343ff4e01f131429878e833b5f39b74
Diniz <185072671+d-branco@users.noreply.github.com>
2024-10-19 11:14:12 +0100

    Improve efficiency

--- ex14 KO/ft_sqrt.c
+++ ex14 KO/ft_sqrt.c
@@ -6,13 +6,15 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 13:46:14 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/10/18 21:36:25 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/10/19 11:13:46 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb);
 
-//////////////////////////////////////////////////////////////////////////////
+/*/////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
 int	main(void)
 {
 	printf("%i\n", ft_sqrt(132443844));
@@ -20,27 +22,25 @@ int	main(void)
 	printf("%i\n", ft_sqrt(0));
 	printf("%i\n", ft_sqrt(1));
 	printf("%i\n", ft_sqrt(2));
+	printf("%i\n", ft_sqrt(3));
 	printf("%i\n", ft_sqrt(4));
-	printf("%i\n", ft_sqrt(132443844));
+	printf("%i\n", ft_sqrt(42));
+	printf("%i\n", ft_sqrt(225));
 }
-//////////////////////////////////////////////////////////////////////////////
+*//////////////////////////////////////////////////////////////////////////////
 
 int	ft_sqrt(int nb)
 {
-	int	half;
 	int	j;
 
-	half = nb / 2;
-	j = 1;
 	if (nb < 1)
 		return (0);
 	if (nb == 1)
 		return (1);
-	while (j <= half)
-	{
-		if (j * j == nb)
-			return (j);
+	j = 1;
+	while (j * j < nb)
 		j++;
-	}
-	return (0);
+	if (j > (nb / j))
+		return (0);
+	return (j);
 }
